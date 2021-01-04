54050f85673241eea5a0865b374c525f0bdba88e
P <phernandeznyc@gmail.com>
2021-01-04 00:02:13 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,7 @@
+display_rainbow = "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+
+
+
 def display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']).
   puts = "R: red, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
 end
